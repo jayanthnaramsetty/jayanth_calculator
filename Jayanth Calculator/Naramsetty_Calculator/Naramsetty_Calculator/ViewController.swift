82798ca7fb555878d6bf9aba15b91bc957d8eea2
Jayanth Naramsetty <naramsetty.jc@gmail.com>
2022-02-10 21:05:27 -0600

    completed dezine and also the connections of buttons
    not yet coded the connections for buttons

--- Jayanth Calculator/Naramsetty_Calculator/Naramsetty_Calculator/ViewController.swift
+++ Jayanth Calculator/Naramsetty_Calculator/Naramsetty_Calculator/ViewController.swift
@@ -8,12 +8,76 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var DisplayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func ac(_ sender: UIButton) {
+    }
+    
+    @IBAction func c(_ sender: UIButton) {
+    }
+    
+    @IBAction func PlusOrMinus(_ sender: UIButton) {
+    }
+    
+    @IBAction func diviedby(_ sender: UIButton) {
+    }
+    
+    @IBAction func seven(_ sender: UIButton) {
+    }
+    
+    @IBAction func eight(_ sender: UIButton) {
+    }
+    
+    @IBAction func nine(_ sender: UIButton) {
+    }
+    
+    @IBAction func multiply(_ sender: UIButton) {
+    }
+    
+    @IBAction func four(_ sender: UIButton) {
+    }
+    
+    @IBAction func five(_ sender: UIButton) {
+    }
+    
+    @IBAction func six(_ sender: UIButton) {
+    }
+    
+    @IBAction func minus(_ sender: UIButton) {
+    }
+    
+    @IBAction func one(_ sender: UIButton) {
+    }
+    
+    @IBAction func second(_ sender: UIButton) {
+    }
+    
+    @IBAction func three(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func plus(_ sender: UIButton) {
+    }
+    
+    @IBAction func zero(_ sender: UIButton) {
+    }
+    
+    @IBAction func decimal(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func percentage(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func equals(_ sender: UIButton) {
+    }
+    
 
 }
 
